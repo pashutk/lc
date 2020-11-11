57abacfc21f9b5ee9b7199eb2c75b2c2c340f9dd
pashutk <me@pashutk.ru>
2020-11-12 00:36:56 +0300

    Untyped: Interpreter

--- untyped-lambda-calculus/main.ts
+++ untyped-lambda-calculus/main.ts
@@ -2,6 +2,7 @@ import * as IS from "./inputStream";
 import { TokenStream } from "./tokenStream";
 import { parse } from "./parser";
 import { pipe } from "fp-ts/lib/pipeable";
+import { Env, evaluate } from "./interpreter";
 
 const program = `# this is a comment
 
@@ -14,19 +15,35 @@ fib = lambda (n) if n < 2 then n else fib(n - 1) + fib(n - 2);
 
 println(fib(15));
 
-print-range = λ(a, b)             # \`λ\` is synonym to \`lambda\`
+printRange = λ(a, b)             # \`λ\` is synonym to \`lambda\`
                 if a <= b then {  # \`then\` here is optional as you can see below
                   print(a);
                   if a + 1 <= b {
                     print(", ");
-                    print-range(a + 1, b);
+                    printRange(a + 1, b);
                   } else println("");        # newline
                 };
-print-range(1, 5);`;
+printRange(1, 5);`;
 
 const program1 = `sum = lambda(a, b) {
   a + b;
 };
-print(sum(1, 2));`;
+print(sum(1, 10));`;
 
-console.log(pipe(program, IS.create, TokenStream, parse));
+const program2 = `sum = lambda(x, y) x + y; print(sum(2, 3));`;
+
+const prog = pipe(program, IS.create, TokenStream, parse);
+
+const globalEnv = new Env();
+
+globalEnv.def("print", (txt) => {
+  process.stdout.write(String(txt));
+  return txt;
+});
+
+globalEnv.def("println", (txt) => {
+  process.stdout.write(String(txt) + "\n");
+  return txt;
+});
+
+evaluate(prog, globalEnv);
