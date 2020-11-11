081f94feb83007d8a50197e3d4400aae2a3afa73
pashutk <me@pashutk.ru>
2020-11-12 01:43:56 +0300

    Untyped: cons, car, cdr, NIL example

--- untyped-lambda-calculus/main.ts
+++ untyped-lambda-calculus/main.ts
@@ -32,7 +32,19 @@ print(sum(1, 10));`;
 
 const program2 = `sum = lambda(x, y) x + y; print(sum(2, 3));`;
 
-const prog = pipe(program, IS.create, TokenStream, parse);
+const program3 = `cons = λ(a, b) λ(f) f(a, b);
+car = λ(cell) cell(λ(a, b) a);
+cdr = λ(cell) cell(λ(a, b) b);
+NIL = λ(f) f(NIL, NIL);
+
+x = cons(1, cons(2, cons(3, cons(4, cons(5, NIL)))));
+println(car(x));                      # 1
+println(car(cdr(x)));                 # 2
+println(car(cdr(cdr(x))));            # 3
+println(car(cdr(cdr(cdr(x)))));       # 4
+println(car(cdr(cdr(cdr(cdr(x))))));  # 5`;
+
+const prog = pipe(program3, IS.create, TokenStream, parse);
 
 const globalEnv = new Env();
 
